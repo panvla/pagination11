671c79eaf8440424f26a57854f32df0f57f7468f
panvla <pandvlado@gmail.com>
2023-04-17 14:28:52 +0200

    asdf

---
+++ paginationiB/src/main/java/com/vladimirpandurov/paginationiB/PaginationiBApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.paginationiB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationiBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationiBApplication.class, args);
+	}
+
+}
---
+++ paginationiB/src/test/java/com/vladimirpandurov/paginationiB/PaginationiBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.paginationiB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationiBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
